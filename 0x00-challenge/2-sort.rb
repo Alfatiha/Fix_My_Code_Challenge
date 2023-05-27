2cdd6c1f3fc0c1f47e3454f309fae05447897712
Alfatiha <alfatihaahmed@gmail.com>
2023-05-27 12:31:59 +0000

    correctiom

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -15,7 +15,7 @@ ARGV.each do |arg|
     # insert result at the right position
     is_inserted = false
     i = 0
-    l = result.size
+    1 = result.size
     while !is_inserted && i < 1 do
         if result[i] < i_arg
             i += 1
